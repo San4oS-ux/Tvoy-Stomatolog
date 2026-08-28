b4bd9e9f6bf7731aef25e58e45216a236947f999
San4oS-ux <san4os17081986@gmail.com>
2026-08-28 23:15:51 +0300

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,588 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-",
+      "name": "1 Фасад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.15984990173803126,
+        "pitch": -0.3346757267560836,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2493519398897277,
+          "pitch": 0.08102165390664418,
+          "rotation": 0,
+          "target": "2-3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.10449565297810182,
+          "pitch": -0.4353120658735037,
+          "title": "Клиника",
+          "text": "\"ТВОЙ СТОМАТОЛОГ\""
+        }
+      ]
+    },
+    {
+      "id": "1-2-",
+      "name": "2 Ресепшен",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.379184929610757,
+          "pitch": 0.6183865520661751,
+          "rotation": 0,
+          "target": "0-1-"
+        },
+        {
+          "yaw": 0.32044246946416344,
+          "pitch": 0.3323129513734031,
+          "rotation": 0,
+          "target": "2-3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-",
+      "name": "3 Ресепшен",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6160032547581018,
+        "pitch": -0.06371995084595916,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2075943584681674,
+          "pitch": 0.4063247461978037,
+          "rotation": 0,
+          "target": "4-5-"
+        },
+        {
+          "yaw": 2.3862043778777142,
+          "pitch": 0.3014551679642228,
+          "rotation": 0,
+          "target": "3-4-"
+        },
+        {
+          "yaw": -2.9543617253868426,
+          "pitch": 0.39145019446679363,
+          "rotation": 0,
+          "target": "0-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4-",
+      "name": "4 Туалет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.929419170866165,
+        "pitch": 0.23515512168624575,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.087690123465311,
+          "pitch": 0.17977821193232302,
+          "rotation": 0,
+          "target": "2-3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5-",
+      "name": "5 Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6374279597317809,
+        "pitch": -0.004069707568925551,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0209053305559195,
+          "pitch": 0.37128429951423314,
+          "rotation": 0,
+          "target": "2-3-"
+        },
+        {
+          "yaw": -1.387514579194093,
+          "pitch": 0.11649309639633998,
+          "rotation": 0,
+          "target": "6-7--"
+        },
+        {
+          "yaw": -0.14955579174402622,
+          "pitch": 0.10233090793429866,
+          "rotation": 0,
+          "target": "7-8--1"
+        },
+        {
+          "yaw": 0.2479179855490834,
+          "pitch": 0.1678650200588212,
+          "rotation": 0,
+          "target": "5-6--2"
+        },
+        {
+          "yaw": 0.1399309232401258,
+          "pitch": 0.03681846823066337,
+          "rotation": 0,
+          "target": "8-9--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6--2",
+      "name": "6 Коридор 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.09842642138401558,
+        "pitch": 0.012964224659571144,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2070099519101376,
+          "pitch": 0.18308512629976903,
+          "rotation": 0,
+          "target": "9-10--3"
+        },
+        {
+          "yaw": 0.6267166075987465,
+          "pitch": 0.10266395939584427,
+          "rotation": 0,
+          "target": "10-11--4"
+        },
+        {
+          "yaw": 1.8834191365475323,
+          "pitch": 0.16634184433014632,
+          "rotation": 0,
+          "target": "11-12--"
+        },
+        {
+          "yaw": -3.123784227764876,
+          "pitch": 0.1560402801862697,
+          "rotation": 0,
+          "target": "8-9--2"
+        },
+        {
+          "yaw": 2.427395494040871,
+          "pitch": 0.05428743414626247,
+          "rotation": 0,
+          "target": "4-5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7--",
+      "name": "7 Рентген кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1224059746986637,
+          "pitch": 0.10327078645334531,
+          "rotation": 0,
+          "target": "4-5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-8--1",
+      "name": "8 Кабинет 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0239412608347713,
+        "pitch": -0.028246828207549868,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8627979529889664,
+          "pitch": 0.2208750628992835,
+          "rotation": 0,
+          "target": "4-5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-9--2",
+      "name": "9 Кабинет 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.8384375770669923,
+        "pitch": 0.18196943976130697,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7435361243231018,
+          "pitch": 0.19705026474653664,
+          "rotation": 0,
+          "target": "4-5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10--3",
+      "name": "10 Кабинет 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.0031432531668649233,
+        "pitch": 0.16046200172658587,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6054831522769577,
+          "pitch": 0.10948144062007614,
+          "rotation": 0,
+          "target": "5-6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11--4",
+      "name": "11 Кабинет 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.837909812735017,
+        "pitch": 0.2655953196539649,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3265510890710317,
+          "pitch": 0.15174064876400806,
+          "rotation": 0,
+          "target": "5-6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12--",
+      "name": "12 Стирилизационная комната",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.017244486601154,
+        "pitch": 0.14323942956597335,
+        "fov": 1.4857831637171537
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6246338329911936,
+          "pitch": 0.1532515907685017,
+          "rotation": 0,
+          "target": "5-6--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
